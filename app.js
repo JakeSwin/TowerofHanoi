b832811c39cbf1c6fc137c08af5fa32bdb88a162
Jacob Swindell <jacob.swindell@btinternet.com>
2020-05-05 18:23:11 +0100

    Finished full functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const columnOne = document.querySelector("#col-one")
 const columnTwo = document.querySelector("#col-two")
 const columnThree = document.querySelector("#col-three")
+const container = document.querySelector(".container")
 
 let hand = {
     html: null,
@@ -13,69 +14,57 @@ let rings = [
 
 let peek = a => a[a.length - 1]
 
-columnOne.addEventListener("click", () => {
-    //If nothing in hand
-    if (hand.value !== null && (hand.value < peek(rings[0]) || peek(rings[0]) === undefined) ) {
+let checkWin = () => {
+    if (arraysEqual(rings[2], [5,4,3,2,1])) {
+        let winMsg = document.createElement("h2")
+        winMsg.textContent = "You Won"
+        winMsg.setAttribute("id", "win-msg")
+        container.prepend(winMsg)
+    }
+}
+
+let arraysEqual = (a, b) => {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+let makeMove = (column, i) => {
+    if (hand.value !== null && (hand.value < peek(rings[i]) || peek(rings[i]) === undefined)) {
         //Add to start of element
-        columnOne.children[0].prepend(hand.html)
-        rings[0].push(hand.value)
+        column.children[0].prepend(hand.html)
+        rings[i].push(hand.value)
 
         hand.html = null
         hand.value = null
+        checkWin()
     } else if (hand.value === null) {
-        //columnOne.children[0].children[0]
-        let TopItem = columnOne.children[0].children[0]
+        //column.children[0].children[0]
+        let TopItem = column.children[0].children[0]
         if (TopItem.classList[0] === "ring") {
             //Remove top child
-            columnOne.children[0].removeChild(TopItem)
+            column.children[0].removeChild(TopItem)
             hand.html = TopItem
-            hand.value = rings[0].pop()
+            hand.value = rings[i].pop()
             //Console.log HTML object
             console.log(hand)
         }
     }
+}
+
+columnOne.addEventListener("click", () => {
+    makeMove(columnOne, 0)
 })
 
 columnTwo.addEventListener("click", () => {
-    if (hand.value !== null && (hand.value < peek(rings[1]) || peek(rings[1]) === undefined) ) {
-        //Add to start of element
-        columnTwo.children[0].prepend(hand.html)
-        rings[1].push(hand.value)
-
-        hand.html = null
-        hand.value = null
-    } else if (hand.value === null) {
-        //columnTwo.children[0].children[0]
-        let TopItem = columnTwo.children[0].children[0]
-        if (TopItem.classList[0] === "ring") {
-            //Remove top child
-            columnTwo.children[0].removeChild(TopItem)
-            hand.html = TopItem
-            hand.value = rings[1].pop()
-            //Console.log HTML object
-            console.log(hand)
-        }
-    }
+    makeMove(columnTwo, 1)
 })
 
 columnThree.addEventListener("click", () => {
-    if (hand.value !== null && (hand.value < peek(rings[2]) || peek(rings[2]) === undefined) ) {
-        //Add to start of element
-        columnThree.children[0].prepend(hand.html)
-        rings[2].push(hand.value)
-
-        hand.html = null
-        hand.value = null
-    } else if (hand.value === null) {
-        //columnThree.children[0].children[0]
-        let TopItem = columnThree.children[0].children[0]
-        if (TopItem.classList[0] === "ring") {
-            //Remove top child
-            columnThree.children[0].removeChild(TopItem)
-            hand.html = TopItem
-            hand.value = rings[2].pop()
-            //Console.log HTML object
-            console.log(hand)
-        }
-    }
+    makeMove(columnThree, 2)
 })
